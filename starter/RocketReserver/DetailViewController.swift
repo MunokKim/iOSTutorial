1d67b83fee09b89fbf22a2f848d6e0abcd432735
Munok <wer0222@naver.com>
2022-04-15 00:04:36 +0900

    6장 완료

--- starter/RocketReserver/DetailViewController.swift
+++ starter/RocketReserver/DetailViewController.swift
@@ -18,6 +18,12 @@ class DetailViewController: UIViewController {
     @IBOutlet private var launchSiteLabel: UILabel!
     @IBOutlet private var bookCancelButton: UIBarButtonItem!
     
+    private var launch: LaunchDetailsQuery.Data.Launch? {
+      didSet {
+        self.configureView()
+      }
+    }
+    
     var launchID: GraphQLID? {
       didSet {
         self.loadLaunchDetails()
@@ -26,23 +32,88 @@ class DetailViewController: UIViewController {
     
     func configureView() {
       // Update the user interface for the detail item.
-      guard
-        let label = self.missionNameLabel,
-        let id = self.launchID else {
-          return
-      }
-
-      label.text = "Launch \(id)"
+        guard
+          self.missionNameLabel != nil,
+          let launch = self.launch else {
+            return
+        }
+        
       // TODO: Adjust UI based on whether a trip is booked or not
+        self.missionNameLabel.text = launch.mission?.name
+        self.title = launch.mission?.name
+
+        let placeholder = UIImage(named: "placeholder")!
+            
+        if let missionPatch = launch.mission?.missionPatch {
+          self.missionPatchImageView.sd_setImage(with: URL(string: missionPatch)!, placeholderImage: placeholder)
+        } else {
+          self.missionPatchImageView.image = placeholder
+        }
+
+        if let site = launch.site {
+          self.launchSiteLabel.text = "Launching from \(site)"
+        } else {
+          self.launchSiteLabel.text = nil
+        }
+            
+        if
+          let rocketName = launch.rocket?.name ,
+          let rocketType = launch.rocket?.type {
+            self.rocketNameLabel.text = "🚀 \(rocketName) (\(rocketType))"
+        } else {
+          self.rocketNameLabel.text = nil
+        }
+            
+        if launch.isBooked {
+          self.bookCancelButton.title = "Cancel trip"
+          self.bookCancelButton.tintColor = .red
+        } else {
+          self.bookCancelButton.title = "Book now!"
+          // Get the color from the main window rather than the view to prevent alerts from draining color
+          self.bookCancelButton.tintColor = UIApplication.shared.windows.first?.tintColor
+        }
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.configureView()
+      super.viewDidLoad()
+
+      self.missionNameLabel.text = "Loading..."
+      self.launchSiteLabel.text = nil
+      self.rocketNameLabel.text = nil
+      self.configureView()
     }
     
     private func loadLaunchDetails() {
-        // TODO: Actually load launch details
+      guard
+        let launchID = self.launchID,
+        launchID != self.launch?.id else {
+          // This is the launch we're already displaying, or the ID is nil.
+          return
+      }
+        
+      Network.shared.apollo.fetch(query: LaunchDetailsQuery(launchId: launchID)) { [weak self] result in
+        guard let self = self else {
+          return
+        }
+        
+        switch result {
+        case .failure(let error):
+          self.showAlert(title: "Network Error",
+                         message: error.localizedDescription)
+        case .success(let graphQLResult):
+          if let launch = graphQLResult.data?.launch {
+            self.launch = launch
+          }
+        
+          if let errors = graphQLResult.errors {
+            let message = errors
+                            .map { $0.localizedDescription }
+                            .joined(separator: "\n")
+            self.showAlert(title: "GraphQL Error(s)",
+                           message: message)
+          }
+        }
+      }
     }
     
     @IBAction private func bookOrCancelTapped() {
@@ -50,6 +121,18 @@ class DetailViewController: UIViewController {
         guard self.isLoggedIn() else {
             self.performSegue(withIdentifier: "showLogin", sender: self)
             return
+        }
+        
+        guard let launch = self.launch else {
+          // We don't have enough information yet to know
+          // if we're booking or cancelling, bail.
+          return
+        }
+            
+        if launch.isBooked {
+          print("Cancel trip!")
+        } else {
+          print("Book trip!")
         }
     }
     
